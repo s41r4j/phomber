6e67e3e7463206424a80232c729b29919e9db5d1
S41R4J <65067289+s41r4j@users.noreply.github.com>
2023-05-20 21:09:52 +0530

    Delete __main__.py

--- .archive/unreleasedv3/__main__.py
+++
@@ -1,9 +0,0 @@
-# Importing main module
-from phomber.phomber import main
-
-# Used to run phomber as cli application
-def run_phomber():
-    try:
-      main()
-    except KeyboardInterrupt:
-      print('[!] Force Terminate')
